01c1baa7ab8356c5aa558873486d7cece1a6135d
Sherif Elsayed <selsayed@ksu.edu>
2019-09-20 17:04:16 -0500

    conversion to Kelvin added

--- temperature.sh
+++ temperature.sh
@@ -8,6 +8,8 @@ echo " "
 
 
 printf "= " 
-echo "scale=2; ($t-32)*5/9+273" | bc
+echo "scale=2; ($t-32)*5/9" | bc
 
-
+# Sherif added Kelvin conversion
+k=$(echo "($t-32)*(5/9)+273.15" | bc -l)
+printf "\nT = %f Kelvin\n\n" "$k"
